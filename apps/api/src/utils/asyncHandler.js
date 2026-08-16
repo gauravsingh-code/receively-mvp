42e6be8e7bcb32fdd56b479ed057b221e72071c3
Raghav <gt824728@gmail.com>
2026-08-16 18:13:32 +0530

    making again on local

--- apps/api/src/utils/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-/**
- * Async Handler - Wraps async route handlers to catch errors
- */
-export const asyncHandler = (fn) => {
-  return (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-};
